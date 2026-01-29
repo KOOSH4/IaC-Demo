981fcee2722bb6e6383440a171234b2286f26381
KOOSH4 <koosha.olad@gmail.com>
2026-01-29 13:41:14 +0100

    add subnet2

--- main.tf
+++ main.tf
@@ -33,7 +33,13 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
-
+# Subnet
+resource "azurerm_subnet" "subnet2" {
+  name                 = "snet-demo2"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
 # Public IP
 resource "azurerm_public_ip" "pip" {
   name                = "pip-demo-vm"
@@ -51,7 +57,7 @@ resource "azurerm_network_interface" "nic" {
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
+    subnet_id                     = azurerm_subnet.subnet2.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.pip.id
   }
